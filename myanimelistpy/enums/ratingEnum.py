ab9e2e819bd3e9cca2ffb3debf0bd5169759c3ac
AllanCapistrano <allan.capistrano3@gmail.com>
2022-07-21 19:31:13 -0300

    Refactor: change enum values

--- myanimelistpy/enums/ratingEnum.py
+++ myanimelistpy/enums/ratingEnum.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 class RatingEnum(Enum):
-    g      = "G - All Ages"
-    pg     = "PG - Children"
-    pg_13  = "pg_13 - Teens 13 and Older"
-    r      = "R - 17+ (violence & profanity)"
-    r_plus = "R+ - Profanity & Mild Nudity" # r+
-    rx     = "Rx - Hentai"
+    g      = "All Ages"
+    pg     = "Children"
+    pg_13  = "Teens 13 and Older"
+    r      = "17+ (violence & profanity)"
+    r_plus = "Profanity & Mild Nudity" # r+
+    rx     = "Hentai"
