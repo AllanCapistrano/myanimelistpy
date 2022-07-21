46083c45a19351c92316dad78f2bd1e66636dd31
AllanCapistrano <allan.capistrano3@gmail.com>
2022-07-20 23:59:12 -0300

    Feat: starting to manipulate json object

--- myanimelistpy/myanimelist.py
+++ myanimelistpy/myanimelist.py
@@ -1,23 +1,50 @@
-from services.validateFields import validateFields
+import requests
+from typing import List
+
+from .services.validateFields import validateFields
+
+# ------------------------------ Constants ----------------------------------- #
+BASE_URL            = "https://api.myanimelist.net/v2"
+ANIME_LIST_ENDPOINT = "anime"
+AUTH_HEADER         = "X-MAL-CLIENT-ID"
+# ---------------------------------------------------------------------------- #
 
 class MyAnimeList:
+    
+    
     def __init__(self, client_id: str) -> None:
         """ Constructor.
+
+        Parameters
+        -----------
+        client_id: :class:`str`
+            MyAnimeList Client ID.
         """
         
         self.client_id = client_id
+        self.base_url  =  BASE_URL
 
     def getAnimeList(
         self, 
-        animeName: str, 
+        anime_name: str, 
         limit: int = 100, 
         offset: int = 0,
-        fields: list = []
+        fields: List[str] = []
     ):
         validateFields(fields=fields)
         
-        try:
-            if(len(fields) > 0):
-                validateFields(fields=fields)
-        except Exception as error:
-            print(error)
+        url = f"{BASE_URL}/{ANIME_LIST_ENDPOINT}?q={anime_name}&limit={limit}&offset={offset}"
+
+        if(len(fields) > 0):
+            url += f"&fields={fields}"
+
+        response = requests.get(
+            url=url,
+            headers={AUTH_HEADER: self.client_id}
+        )
+
+        # json = loads(response.json())
+
+        # print(response.json()["data"][0]["node"]["id"])
+
+        return response.json()
